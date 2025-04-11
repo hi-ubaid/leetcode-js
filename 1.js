901d290c1f5e7703e3b377ffe1b25fc0399bc11d
ubaid <ubaid@advergic.com>
2025-04-11 17:46:07 +0500

    counter

--- 1.js
+++ 1.js
@@ -9,21 +9,10 @@
 // f(); // "Hello World"
 
 // The function returned by createHelloWorld should always return "Hello World".
-
-// Example 2:
-
-// Input: args = [{},null,42]
-// Output: "Hello World"
-// Explanation:
-// const f = createHelloWorld();
-// f({}, null, 42); // "Hello World"
-
 // Any arguments could be passed to the function but it should still always return "Hello World".
 
 //Solution:
-/**
- * @return {Function}
- */
+
 var createHelloWorld = function() {
     
     return function(...args) {
